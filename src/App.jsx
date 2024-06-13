2decea00557a53e8ebe52aa53cd3f4e85c18b2c4
augustopiquer <augusto19001@gmail.com>
2024-06-13 01:23:56 -0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
-import { ItemListContainer } from "./components/ItemListContainer"
-import { Header } from "./components/header/Header"
 import "./css/main.css"
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/header/Header"
+import NotFound from "./components/NotFound.jsx"
+import { ItemListContainer } from "./components/ItemListContainer.jsx"
+import Footer from "./components/Footer.jsx";
+import ItemDetailContainer from "./components/ItemDetailContainer.jsx";
+
 
 function App() {
 
   const numerito = 1;
 
   return (
-    <>
-      <Header numerito={numerito}/>
-      <ItemListContainer />
-    </>
+    <BrowserRouter>
+      <Header numerito={numerito} setNumerito={setNumerito} />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />}/>
+        <Route path="/category/:categoryId" element={<ItemListContainer />}/>
+        <Route path="/item/:itemId" element={<ItemDetailContainer />}/>
+        <Route path="/*" element={<NotFound />}/>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
